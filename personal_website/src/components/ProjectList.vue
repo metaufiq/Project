a5e7b09a3d40e67b46986ecde3d5f897aab10858
Taufiq <32932093+metaufiq@users.noreply.github.com>
2019-04-21 18:45:24 +0700

    update ProjectList

--- personal_website/src/components/ProjectList.vue
+++ personal_website/src/components/ProjectList.vue
@@ -1,30 +1,28 @@
 <template>
     <div id="list">
-        <div id="project">
-            <div id="projectTittle">
-                <h2>MyWorld</h2>
-            </div>
-            <div id="image">
-                <img src="../assets/MyWorld.png" alt="" id="image">
-            </div>
-            <div id="description">
-                <p>MyWorld is a game that implement MineCraft world to website,so you can create and enjoy the world.We use Three.js to create this project</p>
-            </div> 
-        </div>
+        <ProjectTemplate 
+        name='MyWorld' 
+        img='../img/MyWorld.ab56a7dd.png' 
+        desc='MyWorld is a game that implement MineCraft world to website,so you can create and enjoy the world.We use Three.js to create this project'></ProjectTemplate>
 
-        <div id="project">
-            <div id="projectTittle">
-                <h2>This Website too!</h2>
-            </div>
-            <div id="image">
-                <img src="https://cdn.lynda.com/course/82409/82409-636282992381780999-16x9.jpg" alt="" id="image">
-            </div>
-            <div id="description">
-                <p>I use Vue.js to create this website and i hope you like it</p>
-            </div> 
-        </div>
+        <ProjectTemplate
+        name = 'This Website too!'
+        img  = 'https://cdn.lynda.com/course/82409/82409-636282992381780999-16x9.jpg'
+        desc = 'I use Vue.js to create this website and i hope you like it'></ProjectTemplate>
     </div>
 </template>
+
+
+<script>
+import ProjectTemplate from '@/components/template/ProjectTemplate.vue'
+export default {
+    name:'app',
+    components:{
+        ProjectTemplate
+    }
+}
+</script>
+
 
 <style lang="scss" scoped>
     #list{
@@ -33,49 +31,6 @@
         align-items: center;
         text-align: center;
         justify-content: center;
-        #project{
-            display: grid;
-            grid-template-columns: 30% 70%;
-            grid-template-areas: 
-                "tittle tittle"
-                "img description";
-
-            margin-bottom: 3vw;
-            #projectTittle{
-                grid-area: tittle;
-                text-align: left;
-            }
-            #image{
-                grid-area: img;
-                padding-left: 2vw;
-                img{
-                    width: 20vw;
-                    height: 10vw;
-                }
-            }
-
-            #description{
-                grid-area: description;
-                text-align: left;
-                padding-left: 6vw;
-            }
-        }
-    }
-
-    @media screen and (max-width: 800px){
-        #list{
-            #project{
-                grid-template-columns: 20% 80%;
-                #image{
-                    grid-area: img;
-                    padding-left: 2vw;
-                    img{
-                        width: 100%;
-                        height: 100%;
-                    }
-                }
-            }
-        }
     }
 </style>
 
---
+++ personal_website/src/components/template/ProjectTemplate.vue
@@ -0,0 +1,71 @@
+<template>
+    <div id="project">
+            <div id="projectTittle">
+                <h2>{{name}}</h2>
+            </div>
+            <div id="image">
+                <img :src="img" alt="" id="image">
+            </div>
+            <div id="descAndPlay">
+                <!-- <p>MyWorld is a game that implement MineCraft world to website,so you can create and enjoy the world.We use Three.js to create this project</p> -->
+                <p>{{desc}}</p>
+            </div> 
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ProjectTemplate',
+  props: {
+    name: String,
+    desc: String,
+    img: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+        #project{
+            display: grid;
+            grid-template-columns: 30% 70%;
+            grid-template-areas: 
+                "tittle tittle"
+                "img description";
+
+            margin-bottom: 3vw;
+            #projectTittle{
+                grid-area: tittle;
+                text-align: left;
+            }
+            #image{
+                grid-area: img;
+                padding-left: 2vw;
+                img{
+                    width: 20vw;
+                    height: 10vw;
+                }
+            }
+
+            #descAndPlay{
+                grid-area: description;
+                text-align: left;
+                padding-left: 6vw;
+            }
+        }
+
+        @media screen and (max-width: 800px) {
+            #project{
+                grid-template-columns: 50% 60%;
+                #image{
+                    grid-area: img;
+                    padding-left: 2vw;
+                    img{
+                        width: 100%;
+                        height: 90%;
+                    }
+                }
+            }
+        }
+</style>
+
